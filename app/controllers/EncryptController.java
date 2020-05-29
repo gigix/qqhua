fc70636e8c884fa890ac969e9bd6aa00942963e3
Jeff Xiong <gigix1980@gmail.com>
2020-05-29 14:05:53 +0800

    render encoded image in an html page

--- app/controllers/EncryptController.java
+++ app/controllers/EncryptController.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import deaddrop.Basic;
-import deaddrop.core.Image;
 import org.joda.time.DateTime;
 import play.data.FormFactory;
 import play.libs.Files;
@@ -9,25 +7,17 @@ import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 
-import javax.imageio.ImageIO;
 import javax.inject.Inject;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Base64;
 
-import static deaddrop.Basic.decode;
 import static deaddrop.Basic.encode;
 import static java.lang.String.format;
 
 public class EncryptController extends Controller {
     @Inject
     FormFactory formFactory;
-
-    public Result index() {
-        return ok(views.html.encrypt.render());
-    }
 
     public Result upload(Http.Request request) throws IOException {
         String message = formFactory.form().bindFromRequest(request).get("message");
@@ -40,8 +30,7 @@ public class EncryptController extends Controller {
             file.copyTo(Paths.get(originalFilePath), true);
 
             byte[] encryptedImageData = encode(originalFilePath, message);
-            System.out.println(encryptedImageData.length);
-            return ok(encryptedImageData).as("image/png");
+            return ok(views.html.encrypt.render(Base64.getMimeEncoder().encodeToString(encryptedImageData)));
         } else {
             return badRequest().flashing("error", "Missing file");
         }
