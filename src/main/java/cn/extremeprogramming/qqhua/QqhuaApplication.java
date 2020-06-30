702849895c008fb7e97e2658690807fe6cbe312c
Jeff Xiong <gigix1980@gmail.com>
2020-06-30 11:11:25 +0800

    initialize spring app

---
+++ src/main/java/cn/extremeprogramming/qqhua/QqhuaApplication.java
@@ -0,0 +1,13 @@
+package cn.extremeprogramming.qqhua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqhuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QqhuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/extremeprogramming/qqhua/QqhuaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.extremeprogramming.qqhua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqhuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
